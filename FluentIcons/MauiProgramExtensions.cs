b25ea1b9f7dd23a5263ddb30fec813bfea0a9d71
durnwalder <durnwalder@rocketmail.com>
2024-01-19 17:42:24 +0000

    Fix font file names in MauiProgramExtensions.cs

--- FluentIcons/MauiProgramExtensions.cs
+++ FluentIcons/MauiProgramExtensions.cs
@@ -6,16 +6,16 @@ namespace FluentIcons
     public static class MauiProgramExtensions
     {
         internal static readonly string FilledFontFamily = "FluentRegular";
-        internal static readonly string FilledFileName = "Resources.FluentSystemIcons-Filled.ttf";
-        internal static readonly string FilledFontFamily = "FluentFilled";
         internal static readonly string FilledFileName = "Resources.FluentSystemIcons-Regular.ttf";
+        internal static readonly string RegularFontFamily = "FluentFilled";
+        internal static readonly string RegularFileName = "Resources.FluentSystemIcons-Filled.ttf";
 
         public static MauiAppBuilder UseFluentIcons(this MauiAppBuilder builder)
         {
             builder.ConfigureFonts(fonts =>
             {
-                fonts.AddEmbeddedResourceFont((typeof(MauiProgramExtensions).Assembly), FilledFontFamily, FilledFileName);
-                fonts.AddEmbeddedResourceFont((typeof(MauiProgramExtensions).Assembly), RegularFontFamily, RegularFileName);
+                fonts.AddEmbeddedResourceFont(typeof(MauiProgramExtensions).Assembly, FilledFontFamily, FilledFileName);
+                fonts.AddEmbeddedResourceFont(typeof(MauiProgramExtensions).Assembly, RegularFontFamily, RegularFileName);
             });
 
             return builder;
