ddda7de430ed7b9a39513359c8e8cb6364e63268
durnwalder <durnwalder@rocketmail.com>
2024-01-19 17:31:34 +0000

    Update FluentIcons font names and file names

--- FluentIcons/MauiProgramExtensions.cs
+++ FluentIcons/MauiProgramExtensions.cs
@@ -5,21 +5,17 @@ namespace FluentIcons
 {
     public static class MauiProgramExtensions
     {
+        internal static readonly string FilledFontFamily = "FluentRegular";
+        internal static readonly string FilledFileName = "Resources.FluentSystemIcons-Filled.ttf";
+        internal static readonly string FilledFontFamily = "FluentFilled";
+        internal static readonly string FilledFileName = "Resources.FluentSystemIcons-Regular.ttf";
+
         public static MauiAppBuilder UseFluentIcons(this MauiAppBuilder builder)
         {
-            var assembly = typeof(MauiProgramExtensions).Assembly;
-            var filledFontName = "FluentIcons.Resources.FluentSystemIcons-Filled.ttf";
-            var regularFontName = "FluentIcons.Resources.FluentSystemIcons-Regular.ttf";
-
-            if (assembly.GetManifestResourceInfo(filledFontName) == null || assembly.GetManifestResourceInfo(regularFontName) == null)
-            {
-                throw new InvalidOperationException("FluentIcons fonts are not embedded in the assembly.");
-            }
-
             builder.ConfigureFonts(fonts =>
             {
-                fonts.AddFont(filledFontName, "FluentFilled");
-                fonts.AddFont(regularFontName, "FluentRegular");
+                fonts.AddEmbeddedResourceFont((typeof(MauiProgramExtensions).Assembly), FilledFontFamily, FilledFileName);
+                fonts.AddEmbeddedResourceFont((typeof(MauiProgramExtensions).Assembly), RegularFontFamily, RegularFileName);
             });
 
             return builder;
