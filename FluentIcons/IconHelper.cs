bd10cd45fb016191702a47e03f594f1cecc913ef
durnwalder <durnwalder@rocketmail.com>
2024-01-21 18:19:53 +0100

    Refactor GetFontImageSource method signature

--- FluentIcons/IconHelper.cs
+++ FluentIcons/IconHelper.cs
@@ -4,16 +4,12 @@ namespace FluentIcons
 {
     public static class IconHelper
     {
-        public static FontImageSource GetFontImageSource(string iconName, string fontFile)
+        public static FontImageSource GetFontImageSource(string glyph, string fontFile)
         {
-            var glyph = typeof(Filled).GetField(iconName)?.GetValue(null) as string;
-            if (glyph == null) return null;
-
             return new FontImageSource
             {
                 Glyph = glyph,
-                FontFamily = fontFile,
-                Size = 24, // or any other size you want
+                FontFamily = fontFile
             };
         }
     }
